5fa6187b9656eb25cd5e7c4ac1afbb8b48fd57fa
Lucas Costa <lucascosta_s_a@hotmail.com>
2019-10-17 11:01:41 -0300

    criada opração de instanciação

--- src/main/java/com/lucascosta/cursomc/CursomcApplication.java
+++ src/main/java/com/lucascosta/cursomc/CursomcApplication.java
@@ -1,13 +1,37 @@
 package com.lucascosta.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//habilita que a classe execute alguma ação quando a classe iniciar
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.lucascosta.cursomc.domain.Categoria;
+import com.lucascosta.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	//é necessário uma instância do repository para se fazer a ligação com o BD
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	//é necessário que esse método seja criado e aqui é passada a ação a ser executada ao iniciar
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		//através do repository passa uma lista de objetos, criada acima, para o BD
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+      		
 	}
 
 }
