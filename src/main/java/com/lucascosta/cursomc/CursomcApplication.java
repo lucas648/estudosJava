505dfbed6db85918df14e3ccd89ad18c7162d0d6
Lucas Costa <lucascosta_s_a@hotmail.com>
2019-10-17 14:33:32 -0300

    criados estados e cidades

--- src/main/java/com/lucascosta/cursomc/CursomcApplication.java
+++ src/main/java/com/lucascosta/cursomc/CursomcApplication.java
@@ -9,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.lucascosta.cursomc.domain.Categoria;
+import com.lucascosta.cursomc.domain.Cidade;
+import com.lucascosta.cursomc.domain.Estado;
 import com.lucascosta.cursomc.domain.Produto;
 import com.lucascosta.cursomc.repositories.CategoriaRepository;
+import com.lucascosta.cursomc.repositories.CidadeRepository;
+import com.lucascosta.cursomc.repositories.EstadoRepository;
 import com.lucascosta.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -23,6 +27,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -53,12 +61,31 @@ public class CursomcApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-
+		
+		
 		/*
 		 * através do repository passa uma lista de objetos, criada acima, para o BD
 		 */
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
       	produtoRepository.saveAll(Arrays.asList(p1,p2,p3));	
+      	
+      	Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade cid1 = new Cidade(null, "Uberlândia", est1);
+		Cidade cid2 = new Cidade(null, "São Paulo", est2);
+		Cidade cid3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		est2.getCidades().addAll(Arrays.asList(cid2,cid3));
+		
+		/*
+		 * como a relação de estado para cidade é de um para muitos, ou seja
+		 * um estado pode ter várias cidades, os estados devem ser salvos primieros no BD
+		 */
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2,cid3));
+      		
 	}
 
 }
